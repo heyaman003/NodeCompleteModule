5f5cca09cb81926313cf872e35c1231ba1f73c01
Amankr003 <aman.kumar@rthetapi.com>
2023-09-26 17:53:27 +0530

    dhdhhd

--- learningJest/test/fristTest.test.js
+++ learningJest/test/fristTest.test.js
@@ -6,7 +6,7 @@ describe('numerical operation test', () => {
             })
             test('assumeing  a+b should be eqaul to b-a ', () => {
               let a=5;
-              let b=10;
+             
               expect(a+b).not.toBe((b-a));
             })
             
